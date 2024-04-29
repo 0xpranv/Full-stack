574830ad6ebaedee9fb0eed914c31aed5e8c367d
0xpranv <pranav@greendreamearth.org>
2024-04-29 17:03:41 +0530

    m

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -11,11 +11,13 @@ import {
   Carousel,
 } from "@/components/ui/carousel";
 
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+
 import { tenStepProcessData } from "../data/tenStepProcessData";
 
-export const TestimonialsCarousel = () => {
+export const CTA = () => {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
+    Autoplay({ delay: 3000, stopOnInteraction: true })
   );
 
   return (
@@ -62,6 +64,73 @@ const CarouselSlide = ({ item }: { item: any }) => {
         <p className="font-light text-slate-700 text-justify">
           {item.description}
         </p>
+      </div>
+    </CarouselItem>
+  );
+};
+
+export default function TestemonialCarousel() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+
+  return (
+    <div className="py-12 md:py-16 lg:py-18 w-full">
+      <div className="overflow-hidden flex flex-col justify-center items-center">
+        <div className="space-y-4 text-center">
+          <h3 className="text-3xl font-bold sm:text-4xl md:text-5xl">
+            What Our Students Say
+          </h3>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            Hear from our students about their experiences with our program.
+          </p>
+        </div>
+        <Carousel className="w-full" plugins={[plugin.current]}>
+          <CarouselContent>
+            <TestimonialCarouselItem
+              name={"Emma Gonzalez"}
+              major={"Computer TEchnology"}
+              imgSrc={
+                "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df"
+              }
+              testimony={
+                "The professors at this university are truly exceptional. They have challenged me to think critically and have helped me grow both academically and personally."
+              }
+            />
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+const TestimonialCarouselItem = ({
+  name,
+  imgSrc,
+  major,
+  testimony,
+}: {
+  name: string;
+  imgSrc?: string;
+  major: string;
+  testimony: string;
+}) => {
+  return (
+    <CarouselItem className="flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center gap-4 p-6 max-w-5xl">
+        <Avatar className="h-20 w-20">
+          <AvatarImage alt={name} src={imgSrc} className="bg-cover" />
+          <AvatarFallback>{name[0]}</AvatarFallback>
+        </Avatar>
+        <div className="text-center">
+          <h3 className="text-lg font-semibold">{name}</h3>
+          <p className="text-gray-500 dark:text-gray-400">{major}</p>
+          <blockquote className="mt-4 text-lg font-medium leading-relaxed max-w-2xl">
+            &quot;{testimony}&quot;
+          </blockquote>
+        </div>
       </div>
     </CarouselItem>
   );
