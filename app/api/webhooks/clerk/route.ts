fef342b096ad880bb3514b12d888333c1ce25459
0xpranv <pranav@greendreamearth.org>
2024-04-30 20:47:51 +0530

    added GET route and updated getAllUsers function

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { createUser } from "@/lib/actions/user.action";
+import { createUser, getAllUsers } from "@/lib/actions/user.action";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -84,4 +84,10 @@ export async function POST(req: Request) {
   console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
+}
+
+export async function GET() {
+  const allUsers = await getAllUsers();
+  console.log("all users " + allUsers);
+  return NextResponse.json({ users: allUsers });
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -8,8 +8,19 @@ const prisma = new PrismaClient();
 export async function createUser(user: any) {
   try {
     await connect();
-    const newUser = await prisma.user.create(user);
+    const newUser = await prisma.user.create({ data: user });
     return JSON.parse(JSON.stringify(newUser));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getAllUsers() {
+  try {
+    await connect();
+    const newUsers = await prisma.user.findMany({});
+    console.log(newUsers);
+    return newUsers;
   } catch (err) {
     console.log(err);
   }
